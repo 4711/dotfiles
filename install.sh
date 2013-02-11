0ccf5c5eca018e42f489f1312c517826b728bd98
4711 <bzthcyqullt@mailinator.com>
2013-02-11 12:29:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-curl https://github.com/4711/dotfiles/.vimrc       ~/.vimrc
-curl https://github.com/4711/dotfiles/.vimrc.local ~/.vimrc.local
+curl https://raw.github.com/4711/dotfiles/.vimrc       ~/.vimrc
+curl https://raw.github.com/4711/dotfiles/.vimrc.local ~/.vimrc.local
 
