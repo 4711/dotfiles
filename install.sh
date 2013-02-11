bf062c32dbfd6f08c8071d0c4cefbaaf0393a604
4711 <bzthcyqullt@mailinator.com>
2013-02-11 03:24:50 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+curl https://github.com/4711/dotfiles/.vimrc       ~/.vimrc
+curl https://github.com/4711/dotfiles/.vimrc.local ~/.vimrc.local
+
